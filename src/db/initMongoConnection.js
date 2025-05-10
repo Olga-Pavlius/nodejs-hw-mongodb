31490a8a1d5d405cf39d549b163f2765bf372b61
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-05-10 20:04:48 +0300

    add environmental variables

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,18 +1,44 @@
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// export const initMongoConnection = async () => {
+//   try {console.log('MONGODB_URL:', process.env.MONGODB_URL);
+//     await mongoose.connect(process.env.MONGODB_URL, {
+//       dbName: process.env.MONGODB_DB,
+//       user: process.env.MONGODB_USER,
+//       pass: process.env.MONGODB_PASSWORD,
+//     });
+//     console.log('Mongo connection successfully established!');
+//   } catch (error) {
+//     console.error('Mongo connection error:', error.message);
+//     process.exit(1);
+//   }
+// };
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const initMongoConnection = async () => {
-  try {console.log('MONGODB_URL:', process.env.MONGODB_URL);
-    await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: process.env.MONGODB_DB,
-      user: process.env.MONGODB_USER,
-      pass: process.env.MONGODB_PASSWORD,
-    });
+  const {
+    MONGODB_USER,
+    MONGODB_PASSWORD,
+    MONGODB_URL,
+    MONGODB_DB,
+  } = process.env;
+
+  const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
+  try {
+    console.log('MongoDB URI:', uri); // для дебагу, можна прибрати
+    await mongoose.connect(uri);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error.message);
     process.exit(1);
   }
 };
+
