697a612328029ad7b066f05b07919254706b98ce
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-05-11 22:33:54 +0300

    evn

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,22 +1,3 @@
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export const initMongoConnection = async () => {
-//   try {console.log('MONGODB_URL:', process.env.MONGODB_URL);
-//     await mongoose.connect(process.env.MONGODB_URL, {
-//       dbName: process.env.MONGODB_DB,
-//       user: process.env.MONGODB_USER,
-//       pass: process.env.MONGODB_PASSWORD,
-//     });
-//     console.log('Mongo connection successfully established!');
-//   } catch (error) {
-//     console.error('Mongo connection error:', error.message);
-//     process.exit(1);
-//   }
-// };
-
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -30,7 +11,6 @@ export const initMongoConnection = async () => {
     MONGODB_DB,
   } = process.env;
 
-  // Формуємо правильний URI для підключення
   const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
 
