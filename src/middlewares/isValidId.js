c8119fe36cf6cb0ab8c58fb1d6e0d2b9d6da17ef
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-06-02 17:57:45 +0300

    delete commit

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,25 +1,3 @@
-// import { isValidObjectId } from 'mongoose';
-// import createHttpError from 'http-errors';
-
-// export const isValidId = (req, res, next) => {
-//   const { contactId } = req.params;
-//   if (!isValidObjectId(contactId)) {
-//     throw createHttpError(400, 'Bad Request');
-//   }
-
-//   next();
-// };
-
-// import createHttpError from 'http-errors';
-// import { isValidObjectId } from 'mongoose';
-
-// export function isValidID(req, res, next) {
-//   if (isValidObjectId(req.params.id) !== true) {
-//     return next(createHttpError.BadRequest('ID should be an ObjectId'));
-//   }
-
-//   next();
-// }
 import mongoose from 'mongoose';
 
 export function isValidID(req, res, next) {
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,17 +1,3 @@
-// import express from 'express';
-// import {validateBody} from '../middlewares/validateBody.js';
-// import {registerSchema} from '../validation/auth.js';
-// import {ctrlWrapper} from '../utils/ctrlWrapper.js';
-// import {registerController} from '../controllers/auth.controller.js';
-
-// const router = express.Router();
-// const jsonParser = express.json();
-
-// router.post("/register", jsonParser, validateBody(registerSchema), ctrlWrapper(registerController));
-
-// export default router;
-
-
 import express from 'express';
 
 import { validateBody } from '../middlewares/validateBody.js';
